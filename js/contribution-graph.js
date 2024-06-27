1f879a3c3741a22a5543a090099c01967d2101ef
alu1006 <andy8050802@gmail.com>
2024-06-27 14:25:20 +0800

    Site updated: 2024-06-27 14:25:20

---
+++ js/contribution-graph.js
@@ -0,0 +1,48 @@
+// source/js/contribution-graph.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const posts = window.HexoData.posts;
+
+    const formattedData = posts.reduce((acc, post) => {
+        const date = post.date;
+        acc[date] = (acc[date] || 0) + 1;
+        return acc;
+    }, {});
+
+    const data = Object.entries(formattedData).map(([date, count]) => [date, count]);
+
+    const chart = echarts.init(document.getElementById('main'));
+    const option = {
+        tooltip: {
+            position: 'top'
+        },
+        visualMap: {
+            min: 0,
+            max: Math.max(...Object.values(formattedData)),
+            calculable: true,
+            orient: 'horizontal',
+            left: 'center',
+            bottom: '15%'
+        },
+        calendar: {
+            top: 'middle',
+            left: 'center',
+            orient: 'horizontal',
+            cellSize: ['auto', 20],
+            range: new Date().getFullYear(),
+            itemStyle: {
+                borderWidth: 0.5
+            },
+            yearLabel: { show: false },
+            monthLabel: { nameMap: 'cn' },
+            dayLabel: { firstDay: 1, nameMap: 'cn' }
+        },
+        series: [{
+            type: 'heatmap',
+            coordinateSystem: 'calendar',
+            data: data
+        }]
+    };
+
+    chart.setOption(option);
+});

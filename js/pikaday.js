3fa53fc9d84b0a95c5f9a9d132f9601ac13992ed
alu1006 <andy8050802@gmail.com>
2024-09-02 07:18:36 +0800

    Site updated: 2024-09-02 07:18:36

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2021":{"11":{"20":true}},"2024":{"5":{"28":true},"6":{"25":true,"30":true,"31":true},"7":{"30":true,"31":true}}}';
+      var data = '{"2021":{"11":{"20":true}},"2024":{"5":{"28":true},"6":{"25":true,"30":true,"31":true},"7":{"30":true,"31":true},"8":{"2":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
